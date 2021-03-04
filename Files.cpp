80e2c2825c222949ae2c3dd107d1530e82067791
Lalito Robles <laloro08@hotmail.com>
2021-03-04 00:28:55 -0600

    Working with files in cp

---
+++ Files.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  //Now you can use cin and cout normally 
+  return 0;
+}
